921a4d45f8ab6c9ed035a3b4ad62e1a46b276301
Murilo Sanches <murilando@gmail.com>
2020-01-29 13:54:54 -0300

    Design pattern bridge

---
+++ Bridge.Exemplo/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Text;
+
+namespace Bridge.Exemplo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mensagemAdmEmail = new MensagemParaOAdministrador("Murilo", new EnviaPorEmail());
+            mensagemAdmEmail.Envia();
+            var mensagemAdmSms = new MensagemParaOAdministrador("Murilo", new EnviaPorSMS());
+            mensagemAdmSms.Envia();
+
+            var mensagemClienteEmail = new MensagemParaOCliente("José", new EnviaPorEmail());
+            mensagemClienteEmail.Envia();
+            var mensagemClienteSMS = new MensagemParaOCliente("José", new EnviaPorSMS());
+            mensagemClienteSMS.Envia();
+
+            Console.ReadKey();
+        }
+    }
+
+    public interface IEnvia
+    {
+        void Envia(IMensagem mensagem);
+    }
+
+    public interface IMensagem
+    {
+        IEnvia Enviador { get; }
+        void Envia();
+        string FormatarMensagem();
+    }
+
+    public class EnviaPorEmail : IEnvia
+    {
+        public void Envia(IMensagem mensagem)
+        {
+            var mensagemParaEnviar = new StringBuilder();
+            mensagemParaEnviar.AppendLine("Enviando mensagem por e-mail");
+            mensagemParaEnviar.AppendLine(mensagem.FormatarMensagem());
+            Console.WriteLine(mensagemParaEnviar.ToString());
+        }
+    }
+
+    public class EnviaPorSMS : IEnvia
+    {
+        public void Envia(IMensagem mensagem)
+        {
+            var mensagemParaEnviar = new StringBuilder();
+            mensagemParaEnviar.AppendLine("Enviando mensagem por SMS");
+            mensagemParaEnviar.AppendLine(mensagem.FormatarMensagem());
+            Console.WriteLine(mensagemParaEnviar.ToString());
+        }
+    }
+
+    public class MensagemParaOCliente : IMensagem
+    {
+        private string Nome;
+        public IEnvia Enviador { get; private set; }
+
+        public MensagemParaOCliente(string nome, IEnvia enviador)
+        {
+            Nome = nome;
+            Enviador = enviador;
+        }
+
+        public void Envia()
+        {
+            Enviador.Envia(this);
+        }
+
+        public string FormatarMensagem()
+        {
+            return $"Mensagem para o cliente { Nome }";
+        }
+    }
+
+    public class MensagemParaOAdministrador : IMensagem
+    {
+        private string Nome;
+        public IEnvia Enviador { get; private set; }
+
+        public MensagemParaOAdministrador(string nome, IEnvia enviador)
+        {
+            Nome = nome;
+            Enviador = enviador;
+        }
+
+        public void Envia()
+        {
+            Enviador.Envia(this);
+        }
+
+        public string FormatarMensagem()
+        {
+            return $"Mensagem para o administrador { Nome }";
+        }
+    }
+}
